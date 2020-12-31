5f597647a021dec008b5aebd2a665c7189e090b6
fanwucoder <254765517@qq.com>
2020-12-31 16:22:21 +0800

    添加前端相关代码

--- front-end/src/components/HelloWorld.vue
+++ front-end/src/components/HelloWorld.vue
@@ -1,75 +1,83 @@
 <template>
-  <a-table :columns="columns" :data-source="data">
-    <a slot="name" slot-scope="text">{{ text }}</a>
-    <span slot="customTitle"><a-icon type="smile-o" /> Name</span>
-    <span slot="tags" slot-scope="tags">
-      <a-tag
-        v-for="tag in tags"
-        :key="tag"
-        :color="
-          tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'
-        "
-      >
-        {{ tag.toUpperCase() }}
-      </a-tag>
-    </span>
-    <span slot="imgs" slot-scope="imgs">
-      <img
-        v-for="(img,index) in imgs"
-        :key="img"
-        :src="'./static/results/' + img"
-        :style="'padding-left:'+(index*100)+'px;z-index:'+(99+index)"
-        class="myimg"
-      />
-    </span>
+  <a-layout>
+      <a-layout-header></a-layout-header>
+  
+    <a-layout-content>
+        <a-row type="flex" justify="center" align="top">
+    
+      <a-col  :span="20" >
+        <a-table :columns="columns" :data-source="data" size="middle">
+        <a slot="name" slot-scope="text">{{ text }}</a>
 
-    <span slot="action" slot-scope="text, record">
-      <a>Invite 一 {{ record.name }}</a>
-      <a-divider type="vertical" />
-      <a>Delete</a>
-      <a-divider type="vertical" />
-      <a class="ant-dropdown-link"> More actions <a-icon type="down" /> </a>
-    </span>
-  </a-table>
+        <span slot="last_img" slot-scope="last_img">
+         <img :src="last_img" class="last_img"/>
+        </span>
+         <span slot="img" slot-scope="img">
+         <img v-for="im in img" :src="im" />
+        </span>
+        <span slot="is_in_android" slot-scope="is_in_android">
+          {{ is_in_android ? "运行中" : "停止" }}
+        </span>
+
+        <span slot="action" slot-scope="text, record">
+          <a>启动</a>
+          <a-divider type="vertical" />
+          <a v-on:click="test">停止</a>
+          <a-divider type="vertical" />
+          <a> 删除 </a>
+        </span>
+      </a-table>
+      </a-col>
+
+    </a-row>
+
+
+      
+      </a-layout-content    >
+       <a-layout-footer></a-layout-footer>
+  </a-layout>
 </template>
 <style scoped>
-.myimg {
-  position: absolute;
-   clip-path: polygon(50px 0px, 50px 50px, 100px 50px, 100px 0px);
+.ant-layout-header{
+  background: #ffffff;
+}
+.last_img{
+    width: 160px;
+    height: 90px;
 }
 </style>
 <script>
 const columns = [
   {
+    title: "序号",
+    dataIndex: "index",
+    key: "index",
+  },
+  {
+    title: "名字",
     dataIndex: "name",
     key: "name",
-    slots: { title: "customTitle" },
-    scopedSlots: { customRender: "name" },
   },
   {
-    title: "Age",
-    dataIndex: "age",
-    key: "age",
+    title: "运行状态",
+    dataIndex: "is_in_android",
+    key: "is_in_android",
+    scopedSlots: { customRender: "is_in_android" },
   },
   {
-    title: "Imgs",
-    dataIndex: "imgs",
-    key: "imgs",
-    scopedSlots: { customRender: "imgs" },
+    title: "截图",
+    dataIndex: "last_img",
+    key: "last_img",
+      scopedSlots: { customRender: "last_img" },
   },
   {
-    title: "Address",
-    dataIndex: "address",
-    key: "address",
+    title: "状态截图",
+    key: "img",
+    dataIndex: "img",
+     scopedSlots: { customRender: "img" },
   },
   {
-    title: "Tags",
-    key: "tags",
-    dataIndex: "tags",
-    scopedSlots: { customRender: "tags" },
-  },
-  {
-    title: "Action",
+    title: "操作",
     key: "action",
     scopedSlots: { customRender: "action" },
   },
@@ -80,39 +88,27 @@ const images = [
   "11_20201230122704_start_login_ok.png",
   "11_20201230122704_start_run.png",
 ];
-const data = [
-  {
-    key: "1",
-    name: "John Brown",
-    age: 32,
-    imgs: images,
-    address: "New York No. 1 Lake Park",
-    tags: ["nice", "developer"],
-  },
-  {
-    key: "2",
-    name: "Jim Green",
-    age: 42,
-    imgs: images,
-    address: "London No. 1 Lake Park",
-    tags: ["loser"],
-  },
-  {
-    key: "3",
-    name: "Joe Black",
-    age: 32,
-    imgs: images,
-    address: "Sidney No. 1 Lake Park",
-    tags: ["cool", "teacher"],
-  },
-];
 
 export default {
   data() {
     return {
-      data,
+      props: {
+        rowKey: "index",
+      },
+      data: [],
       columns,
     };
+  },
+  mounted: function () {
+    this.refresh();
+  },
+  methods: {
+    refresh: function () {
+      this.$axios.get("/list").then((res) => (this.data = res.data));
+    },
+    test: function (evt) {
+      this.$axios.get("/").then((res) => alert(res.data));
+    },
   },
 };
 </script>
--- front-end/src/main.js
+++ front-end/src/main.js
@@ -6,6 +6,11 @@ import router from './router'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 import App from './App'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:5000';
+// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+Vue.prototype.$axios=axios
 Vue.config.productionTip = false
 
 Vue.use(Antd)
