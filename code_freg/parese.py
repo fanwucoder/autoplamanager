8a438027159cfc9d8559f83ce1c656682956da36
fanwucoder <254765517@qq.com>
2021-01-05 16:57:21 +0800

    临时代码片段

---
+++ code_freg/parese.py
@@ -0,0 +1,11 @@
+
+# -*- coding: utf-8 -*-
+from lxml import etree
+
+a=etree.parse("ui.xml")
+data=a.xpath("//node[@text='main.lua']")[0]
+print(data.attrib.get("bounds"))
+
+b=etree.parse("ui1.xml")
+data=b.xpath("//node[@text='立即运行']")[0]
+print(data.attrib.get("bounds"))
