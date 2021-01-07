b592c00ea7f3707cbfa8dd4f3f18f848b5f35757
fan <254765517@qq.com>
2021-01-07 23:11:54 +0800

    按需加载模块 直接打包前端文件到目录

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -3,20 +3,26 @@
 import Vue from 'vue'
 
 import router from './router'
-import Antd from 'ant-design-vue'
+import {Button, Modal, Layout, Table} from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css'
 import App from './App'
 import axios from 'axios'
 import qs from 'qs'
 
-axios.defaults.baseURL = 'http://127.0.0.1:5000';
+// axios.defaults.baseURL = 'http://127.0.0.1:5000';
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 Vue.prototype.$axios = axios
 Vue.prototype.$qs = qs
 Vue.config.productionTip = false
 
-Vue.use(Antd)
+Vue.component(Button.name, Button)
+Vue.component(Modal.name, Modal)
+Vue.component(Layout.name, Layout)
+Vue.component(Table.name, Table)
+
+
+// Vue.use(Antd)
 
 /* eslint-disable no-new */
 new Vue({
